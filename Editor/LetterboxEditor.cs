36b370f5611e1d10af33f39ee7ab6715825ad57a
Rodrigo Saguer Pacheco <49574554+Extelen@users.noreply.github.com>
2021-05-21 22:13:19 -0400

    Implemented Letterbox.

---
+++ Editor/LetterboxEditor.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using Extelen.UI;
+
+[CustomEditor(typeof(Letterbox))]
+public class LetterboxEditor : Editor {
+	
+	//Set Params
+	private SerializedProperty m_letterboxRect = null;
+	private SerializedProperty m_topBarRect = null;
+	private SerializedProperty m_bottomBarRect = null;
+
+	private SerializedProperty m_defaultActive = null;
+	private SerializedProperty m_useStandard = null;
+	private SerializedProperty m_barViewportSize = null;
+		        
+	private SerializedProperty m_unscaledTime = null;
+	private SerializedProperty m_activationSmoothness = null;
+	private SerializedProperty m_activationTime = null;
+
+    //Methods
+	private void OnEnable() {
+
+		m_letterboxRect = serializedObject.FindProperty("m_letterboxRect");
+		m_topBarRect = serializedObject.FindProperty("m_topBarRect");
+		m_bottomBarRect = serializedObject.FindProperty("m_bottomBarRect");
+
+		m_defaultActive = serializedObject.FindProperty("m_defaultActive");
+		m_useStandard = serializedObject.FindProperty("m_useStandard");
+		m_barViewportSize = serializedObject.FindProperty("m_barViewportSize");
+
+		m_unscaledTime = serializedObject.FindProperty("m_unscaledTime");
+		m_activationSmoothness = serializedObject.FindProperty("m_activationSmoothness");
+		m_activationTime = serializedObject.FindProperty("m_activationTime");
+		}
+
+	public override void OnInspectorGUI() {
+		
+		serializedObject.Update();
+		
+		EditorGUILayout.PropertyField(m_letterboxRect);
+
+		bool m_canvasIsAssigned = m_letterboxRect.objectReferenceValue;
+
+		if (m_canvasIsAssigned) {
+
+			EditorGUILayout.PropertyField(m_topBarRect);
+			EditorGUILayout.PropertyField(m_bottomBarRect);
+
+			bool m_topBarIsAssigned = m_topBarRect.objectReferenceValue;
+			bool m_bottomBarIsAssigned = m_bottomBarRect.objectReferenceValue;
+
+			if (m_topBarIsAssigned || m_bottomBarIsAssigned) {
+
+				if ((m_topBarIsAssigned && !m_bottomBarIsAssigned) || (!m_topBarIsAssigned && m_bottomBarIsAssigned)) {
+
+					EditorGUILayout.HelpBox("It is recommended that you have the two bars assigned.", MessageType.Warning);
+					}
+
+				EditorGUILayout.PropertyField(m_defaultActive);
+				EditorGUILayout.PropertyField(m_useStandard);
+					
+				if (!m_useStandard.boolValue) {
+
+					EditorGUILayout.PropertyField(m_barViewportSize);
+					}
+				else {
+
+					Letterbox m_letterbox = (Letterbox) target;
+					float m_aspectRatio = m_letterbox.AspectRatio;
+					string m_aspectRatioString = m_letterbox.AspectRatio.ToString("N2");
+
+					if (m_aspectRatioString == "1.78") m_aspectRatioString = m_aspectRatioString + " (16:9)";
+					if (m_aspectRatioString == "1.32") m_aspectRatioString = m_aspectRatioString + " (4:3)";
+
+					float m_viewPortSize = m_letterbox.StandardViewportBarSize;
+					string m_viewPortSizeString = m_viewPortSize.ToString("N3");
+
+					string m_clampMessage = ".";
+
+					if (m_viewPortSize == 0.075f) m_clampMessage = "(The viewport size is clamped to 0.075).";
+					else if (m_viewPortSize == 0.25f) m_clampMessage = "(The viewport size is clamped to 0.25).";
+
+					EditorGUILayout.HelpBox("The cinematic standard aspect ratio is 2.35 (21:9) and the current aspect ratio is " + m_aspectRatioString + ", this will make the size of each letterbox be "+ m_viewPortSizeString +" viewport units " + m_clampMessage, MessageType.Info);
+					}	
+
+				EditorGUILayout.PropertyField(m_unscaledTime);
+				EditorGUILayout.PropertyField(m_activationSmoothness);
+				EditorGUILayout.PropertyField(m_activationTime);
+				}
+
+			else {
+
+				EditorGUILayout.HelpBox("You do not have any bar assigned, to use the letterbox you need to have at least one.", MessageType.Error);
+				}
+			}
+		
+		else {
+			
+			EditorGUILayout.HelpBox("Canvas property is not assigned.", MessageType.Error);
+			}
+
+		serializedObject.ApplyModifiedProperties();
+		}	
+	}
---
+++ Runtime/Letterbox.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extelen.Utilities.Animation;
+
+namespace Extelen.UI {
+
+	[ExecuteAlways]
+	[RequireComponent(typeof(RectTransform))]
+	public class Letterbox : MonoBehaviour {
+			
+		//Params
+					
+			//Non Static
+			[Header("Letterbox References")]
+			[SerializeField] private RectTransform m_letterboxRect = null;
+			[SerializeField] private RectTransform m_topBarRect = null;
+			[SerializeField] private RectTransform m_bottomBarRect = null;
+
+			[Header("Letterbox Values")]
+			[SerializeField] private bool m_defaultActive = false;
+			[SerializeField] private bool m_useStandard = true;
+			[SerializeField] [Range(0.075f, 0.25f)] private float m_barViewportSize = 0.1f;
+
+			[Header("Letterbox Animation")]
+			[SerializeField] private bool m_unscaledTime = true;
+			[SerializeField] private AnimationCurve m_activationSmoothness = DefaultCurves.SmoothInOut;
+			[SerializeField] private float m_activationTime = 0.25f;
+
+			public float AspectRatio {
+
+				get => m_letterboxRect.rect.width / m_letterboxRect.rect.height;
+				}
+			public float StandardViewportBarSize {
+				
+				get {
+					
+					float m_standard = 21f/9f;
+					return Mathf.Clamp((m_letterboxRect.rect.height / m_standard) / (m_letterboxRect.rect.width * 2), 0.075f, 0.25f); 
+					}
+				}
+
+			private Coroutine m_letterboxRoutine = null;
+			private bool m_letterboxActive = false;
+			private float m_letterboxValue = 0;
+
+		//Methods
+		
+			//Non Static
+
+				//MonoBehaviour Methods
+				private void OnValidate() {
+
+					if (m_letterboxRect == null) TryGetComponent<RectTransform>(out m_letterboxRect);
+					m_barViewportSize = Mathf.Clamp(m_barViewportSize, 0.05f, 0.25f);
+
+					UpdateBarScale(m_defaultActive ? 1 : 0);
+					}
+				private void Start() {
+
+					m_letterboxActive = m_defaultActive;
+					m_letterboxValue = m_defaultActive ? 1 : 0;
+
+					UpdateBarAnchors();
+					UpdateBarScale(m_defaultActive ? 1 : 0);
+					}
+				private void Update() {
+						
+					if (!Application.IsPlaying(gameObject)) {
+
+						if (m_bottomBarRect && (m_topBarRect || m_letterboxRect)) {
+							
+							UpdateBarAnchors();
+							}
+						}
+					}
+				
+				//Change Anchors
+				private void UpdateBarAnchors() => UpdateBarAnchors(m_useStandard ? StandardViewportBarSize : m_barViewportSize);
+				private void UpdateBarAnchors(float barViewportSize) {
+
+					if (m_topBarRect != null) {
+						
+						m_topBarRect.anchorMin = new Vector2(0, 1 - barViewportSize);
+						m_topBarRect.anchorMax = Vector2.one;
+						}
+					if (m_bottomBarRect != null) {
+						
+						m_bottomBarRect.anchorMin = Vector2.zero;
+						m_bottomBarRect.anchorMax = new Vector2(1, barViewportSize);
+						}
+					}
+					
+				public void ChangeBarSize(bool cinematographicStandard) {
+					
+					m_useStandard = cinematographicStandard;
+
+					if (cinematographicStandard) {
+						
+						UpdateBarAnchors(StandardViewportBarSize);
+						}
+
+					else {
+
+						UpdateBarAnchors(m_barViewportSize);
+						}
+					}
+				public void ChangeBarSize(float barViewportSize) {
+
+					m_useStandard = false;
+					UpdateBarAnchors(barViewportSize);
+					}
+
+				//Bars Scale
+				public void ActiveLetterbox() => SetLetterboxActive(true);
+				public void DeactiveLetterbox() => SetLetterboxActive(false);
+				public void ToggleLetterbox() => SetLetterboxActive(!m_letterboxActive);
+					
+				public void SetLetterboxActive(bool active) => SetLetterboxActive(active, false);
+				public void SetLetterboxActive(bool active, bool overwrite) {
+
+					if (m_letterboxActive == active && !overwrite) return;
+					m_letterboxActive = active;
+
+					if (active) ActiveLetterbox();
+					else DeactiveLetterbox();
+					}
+
+				private void UpdateBarScale(float value) {
+					
+					value = Mathf.Clamp01(value);
+
+					Vector3 m_scale = new Vector3(1, value, 1);
+					if (m_topBarRect != null) m_topBarRect.localScale = m_scale;
+					if (m_bottomBarRect != null) m_bottomBarRect.localScale = m_scale;
+					}
+
+				private void RunRoutine(bool active) => CoroutineUtilites.OverwriteRoutine(LetterboxRoutine(active), ref m_letterboxRoutine, this);
+				
+
+		//Coroutines
+		private IEnumerator LetterboxRoutine(bool active) {
+			
+			float m_startValue = m_letterboxValue;
+			float m_finalValue = active ? 1 : 0;
+
+			for(float i = 0; i < m_activationTime; i += m_unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime) {
+				
+				UpdateBarScale(Mathf.Lerp(m_startValue, m_finalValue, m_activationSmoothness.Evaluate(i / m_activationTime)));
+				yield return null;
+				}
+
+			UpdateBarScale(m_finalValue);
+			}
+		}
+	}
